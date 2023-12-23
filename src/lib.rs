b9f4cb3cf80de30688b0b2336fc9e8ceff0dd694
Jules Omlor <jules.omlor@remote.com>
2023-12-22 22:26:04 -0500

    parse form mvp

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
-#![allow(unused_imports)]
+#![allow(dead_code)]
 
-use actix_web::{ web, App, HttpRequest, HttpServer, HttpResponse, Responder };
+use actix_web::{ web, App, HttpServer, HttpResponse };
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// }
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            // .route("/", web::get().to(greet))
-            // .route("/{name}", web::get().to(greet))
             .route("/health-check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        // .bind(address)?
         .listen(listener)?
         .run();
 
